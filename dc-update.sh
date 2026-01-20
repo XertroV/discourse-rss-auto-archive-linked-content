905de29e1c7963b927545b5f17a08b63b079698b
Max Kaye <m@xk.io>
2026-01-21 04:40:55 +1100

    feat: Enhance dc-update.sh to include Docker cleanup commands

--- dc-update.sh
+++ dc-update.sh
@@ -7,3 +7,8 @@ git pull --ff-only
 
 ./dc-rebuild.sh
 ./dc-restart.sh
+
+# clean up dangling images & docker usage
+docker image prune -f
+docker buildx prune --filter "until=12h" -f
+docker builder prune -a --filter until=12h -f
