7ecde6b69c876847f92de06477f9932e1ecdd1cc
Max Kaye <m@xk.io>
2026-01-22 20:22:09 +1100

    fix domain

--- dev-cp-db-from-server.sh
+++ dev-cp-db-from-server.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 # Copy production database from server for local development
 cd "$(dirname "$0")"
 
-ssh cf-archive.xk.io "./svr-copy-db.sh"
-scp cf-archive.xk.io:discourse-rss-auto-archive-linked-content/archive.sqlite ./archive.sqlite
+ssh cf-archiver.xk.io "discourse-rss-auto-archive-linked-content/svr-copy-db.sh"
+scp cf-archiver.xk.io:discourse-rss-auto-archive-linked-content/archive.sqlite ./archive.sqlite
